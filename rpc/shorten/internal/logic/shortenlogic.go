b018679fad19d4d72ea71e81534cd3a9d367adb4
zhengyu <949812478@qq.com>
2020-08-31 12:27:50 +0800

    feather(rpc:shorten):finish the service

--- rpc/shorten/internal/logic/shortenlogic.go
+++ rpc/shorten/internal/logic/shortenlogic.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"github.com/tal-tech/go-zero/core/hash"
+	"shorturl/rpc/model"
 
 	"shorturl/rpc/shorten/internal/svc"
 	shorten "shorturl/rpc/shorten/pb"
@@ -9,20 +11,33 @@ import (
 	"github.com/tal-tech/go-zero/core/logx"
 )
 
+const keyLen = 6
+
 type ShortenLogic struct {
 	ctx context.Context
 	logx.Logger
+	model *model.ShorturlModel
 }
 
 func NewShortenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShortenLogic {
 	return &ShortenLogic{
 		ctx:    ctx,
 		Logger: logx.WithContext(ctx),
+		model:  svcCtx.Model,
 	}
 }
 
 func (l *ShortenLogic) Shorten(in *shorten.ShortenReq) (*shorten.ShortenResp, error) {
-	// todo: add your logic here and delete this line
+	key:=hash.Md5Hex([]byte(in.Url))[:keyLen]
+	_,err:=l.model.Insert(model.Shorturl{
+		Shorten: key,
+		Url:     in.Url,
+	})
+	if err!=nil{
+		return nil,err
+	}
 
-	return &shorten.ShortenResp{}, nil
+	return &shorten.ShortenResp{
+		Key: key,
+	}, nil
 }
--- rpc/shorten/internal/svc/servicecontext.go
+++ rpc/shorten/internal/svc/servicecontext.go
@@ -1,13 +1,19 @@
 package svc
 
-import "shorturl/rpc/shorten/internal/config"
+import (
+	"github.com/tal-tech/go-zero/core/stores/sqlx"
+	"shorturl/rpc/model"
+	"shorturl/rpc/shorten/internal/config"
+)
 
 type ServiceContext struct {
 	c config.Config
+	Model *model.ShorturlModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		c: c,
+		c:     c,
+		Model: model.NewShorturlModel(sqlx.NewMysql(c.DataSource), c.Cache, c.Table),
 	}
 }
